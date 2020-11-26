5410299899cc28d00dbff05dced3a7f92bbd7889
Hins Chiu <chiuhi2@oocl.com>
2020-11-26 17:14:20 +0800

    [marsRover][HinsChiu]test:implement test for x minus 1 when process command given (0,0,W) and M

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -40,4 +40,16 @@ class MarsRoverTest {
         assertEquals(1, marsRover.getxLocation());
         assertEquals("E", marsRover.getDirection());
     }
+
+    @Test
+    void should_x_minus_1_when_process_command_given_0_0_W_M() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0,"W");
+        //when
+        marsRover.processCommands("M");
+        //then
+        assertEquals(0, marsRover.getyLocation());
+        assertEquals(-1, marsRover.getxLocation());
+        assertEquals("W", marsRover.getDirection());
+    }
 }
